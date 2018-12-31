ce62d6a998eb8506aa321be50069c1695e64a715
QAlexBall <zhuderen12345@icloud.com>
2018-12-31 23:43:08 +0800

    update

--- Interfaces/interfaces-are-satisfied-implicitly.go
+++ Interfaces/interfaces-are-satisfied-implicitly.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+// part interface-values.go
+
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
+func main() {
+	var i I = T{"hello"}
+	i.M()
+
+	var i1 I
+
+	i1 = &T{"Hello"}
+	describe(i1)
+	i1.M()
+
+	i1 = F(math.Pi)
+	describe(i1)
+	i1.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
--- Methods/use_package.go
+++ Methods/use_package.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	mylib.Test()
 	vertex := mylib.MyVertex{X: 3, Y: 4}
 	fmt.Println(vertex.X)
 	vertex.Show()
