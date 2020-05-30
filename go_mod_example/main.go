9f854e23a52ed2e56a25032cf5d3340b7a41334e
qalexball <zhuderen12345@icloud.com>
2020-05-31 00:44:09 +0800

    add go mod example

---
+++ go_mod_example/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"chris/hello"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(hello.Hello())
+	return
+}
--- src/hello/go.mod
+++
@@ -1,5 +0,0 @@
-module chris/hello
-
-go 1.13
-
-require rsc.io/quote/v3 v3.1.0
